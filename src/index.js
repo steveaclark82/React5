8c552f890151180965ee24b5359f3e161afe15c8
Steve Clark <steve.a.clark48@gmail.com>
2022-02-16 09:21:25 -0600

    Added links and routes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-alert("it's alive");
+import React from 'react';
+import ReactDOM, { render } from 'react-dom';
+import Home from './screens/Home';
+import About from './screens/About';
+import FavoriteThings from './screens/Favoritethings';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+
+const App = ()=> (
+    <Router>
+        <div>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/favoriteThings">Favorite Things</Link></li>
+            </ul>
+
+        <hr />
+            <Route exact path="/" component={Home}/>
+            <Route path="/about"component={About}/>
+            <Route path="/favoriteThings" component={FavoriteThings}/>
+        </div>
+    </Router>
+);
+
+ReactDOM.render(<App/>, document.getElementById('root'));
